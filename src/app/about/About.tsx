d4da0c3d139192cec8dabe02ac0e71508c9857ce
ragu0407 <ragunath0407@gmail.com>
2025-03-20 10:04:26 +0530

    new codes

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import feature from "../../assets/features/banner.svg";
 
 const About = () => {
+
   
   return (
     <section className="section features" id="features" aria-label="features">
