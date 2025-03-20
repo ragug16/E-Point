2bb9b9869d6e11d8bd927793750270f39e5b4663
ragu0407 <ragunath0407@gmail.com>
2025-03-20 14:45:07 +0530

    new codes

--- src/app/about/About.tsx
+++ src/app/about/About.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import feature from "../../assets/features/banner.svg";
 
 const About = () => {
+  
   return (
     <section className="section features" id="features" aria-label="features">
       <div className="container mx-auto px-4 text-center">
